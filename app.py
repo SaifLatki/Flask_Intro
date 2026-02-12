03786137204eb60dce0654534f3d0ca93216636a
Saif <saifullahlatki338@gmail.com>
2026-02-13 02:45:34 +0500

    done with session and permanent sessions also

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask,redirect, request,url_for,render_template
+from flask import Flask,redirect, request,url_for,render_template,session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=5)
 
 @app.route("/")
 def home():
@@ -17,14 +20,26 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["username"]
+        session["username"] = user
         return redirect(url_for("user", usr=user))
     else:
+       if "username" in session:
+            return redirect(url_for("user"))
        return render_template("login.html")
-@app.route("/<usr>") 
-def user(usr):
-    return f"<h1>Hello, {usr}!</h1>"
+@app.route("/user") 
+def user():
+    if "username" in session:
+        usr = session["username"]
+        return f"<h1>{usr}</h1>"
+    else:
+        return redirect(url_for("login"))
 
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run( debug=True)
